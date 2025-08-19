f3f9018a813a42dbb55b6bf86d78f9d3fca38a0b
Codexign <codexignllc@gmail.com>
2025-08-19 22:56:35 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/blog-generator-form.tsx
+++ src/components/blog-generator-form.tsx
@@ -1,6 +1,7 @@
 
 'use client';
 
+import * as React from 'react';
 import { useState, useEffect, useRef, useCallback } from 'react';
 import { useForm, useFieldArray } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
